a5c3a8496f0ac699a158a1676609d170b2e26806
fozia <foziakassa19@gmail.com>
2025-03-07 09:08:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express= require('express')
+const app = express()
+const port =process.env.PORT||3000
+app.get('/', (req, res)=>{
+    res.send('<h1>Hi there </h1>')
+
+})
+app.listen(port ,()=>console.log(`listining on port ${port}`))
