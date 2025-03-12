caefbc6a5061b23db24221b8cd8b76eb52388834
fozia <foziakassa19@gmail.com>
2025-03-12 17:04:40 +0300

    comm

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { Pool } = require("pg");
-const bcrypt = require("bcrypt");
-require("dotenv").config();
-
+const express = require('express');
 const app = express();
+const port = 3000;
 
+app.get('/message', (req, res) => {
+    res.send('Hi there!');
+});
 
-
-app.get('/', (req, res)=>{
-    res.send("hello there")
-})
-app.listen(process.env.PORT)
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
