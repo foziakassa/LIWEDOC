a3b9be06ad864ddedb29a4a4fc9dbb7b7ed059dd
fozia <foziakassa19@gmail.com>
2025-03-12 17:05:33 +0300

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/message', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hi there!');
 });
 
