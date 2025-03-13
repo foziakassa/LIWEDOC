451616f5ce6d347b2e85e6f7cd039937f6181351
fozia <foziakassa19@gmail.com>
2025-03-13 10:58:49 +0300

    routechane

---
+++ api/products.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/message', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
