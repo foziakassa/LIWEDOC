52adba9988330af2fce25fc6f7dedcdf17514087
fozia <foziakassa19@gmail.com>
2025-03-13 12:17:55 +0300

    up

--- api/users.js
+++ api/users.js
@@ -9,13 +9,11 @@ router.use(bodyParser.json());
 
 // PostgreSQL connection setup
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-
 // GET API for users
 router.get("/", (req, res) => {
     res.send("User endpoint");
