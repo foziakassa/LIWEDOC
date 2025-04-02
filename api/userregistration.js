a555c50b79702525b7698cf7aa6bbcd607e503a3
fozia <foziakassa19@gmail.com>
2025-04-02 12:14:37 +0300

    bb

--- api/userregistration.js
+++ api/userregistration.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // PostgreSQL connection setup
 const pool = new Pool({
     // connectionString: process.env.DATABASE_URL,
-    connectionString: process.env.bd_url,
+    connectionString: process.env.DATABASE_URL,
 
     ssl: {
         rejectUnauthorized: false,
