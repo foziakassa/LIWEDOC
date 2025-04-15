b7be6f756e6e45ed1f97177709589f5290734437
fozia <foziakassa19@gmail.com>
2025-04-15 15:18:01 +0300

    column Role added code ajusted accordingly

--- api/userregistration.js
+++ api/userregistration.js
@@ -62,7 +62,7 @@ app.get("/users", async (req, res) => {
 
 // POST route to create a new user
 app.post("/users", async (req, res) => {
-    const { Firstname, Lastname, Email, Password } = req.body;
+    const { Firstname, Lastname, Email, Password , Role } = req.body;
 
     if (!Firstname || !Lastname || !Email || !Password) {
         return res.status(400).json({ error: "All fields are required." });
@@ -76,8 +76,8 @@ app.post("/users", async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(Password, 10);
         const newUser = await pool.query(
-            "INSERT INTO \"user\" (\"Firstname\", \"Lastname\", \"Email\", \"Password\", \"Createdat\") VALUES ($1, $2, $3, $4, NOW()) RETURNING *",
-            [Firstname, Lastname, Email, hashedPassword]
+            "INSERT INTO \"user\" (\"Firstname\", \"Lastname\", \"Email\", \"Password\",\"Role\", \"Createdat\" ) VALUES ($1, $2, $3, $4, $5, NOW()) RETURNING *",
+            [Firstname, Lastname, Email, hashedPassword ,Role]
         );
 
         console.log("New user created:", newUser.rows[0]); // Logging the created user
